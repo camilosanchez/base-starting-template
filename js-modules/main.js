1ec6483d5a429fe9b2a7a5a14185756021bfa953
Chris Vargas Pérez <chrisvargas@Chris.local>
2018-03-12 17:23:42 -0400

    Chris Version 1.0

--- js-modules/main.js
+++
@@ -1,3 +0,0 @@
-console.log("hello from the main js file! - Please delete this line.");
-
-require('./test.js');
--- js-modules/test.js
+++
@@ -1,1 +0,0 @@
-console.log("hello again from a required js file! - Please delete this line and this file.");
